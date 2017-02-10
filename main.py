765c499cc81cd6a86fed655ce4ffea5b87c4633d
rguelot <rguelot@rapids.local>
2017-02-09 23:30:57 -0500

    junk

--- main.py
+++ main.py
@@ -280,7 +280,7 @@ class OAuthHandler(webapp2.RequestHandler):
 		new_user = User(stateXSRF=state)
 		new_user.put
 		#url = "https://www.google.com"
-		url = 'https://www.accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=' + clientId + '&redirect_uri=' + redirectUri + '&scope=email&state=' + state
+		url = "https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=" + clientId + "&redirect_uri=" + redirectUri + "&scope=email&state=" + state
 		result = urlfetch.fetch(url)
 		self.response.write(result)
 
