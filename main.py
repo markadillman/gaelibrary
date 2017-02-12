88c2d92be7ff7138664ea4893a86fb7dc37a299a
rguelot <rguelot@rapids.local>
2017-02-11 21:36:26 -0500

    hibby

--- main.py
+++ main.py
@@ -296,15 +296,8 @@ class OAuthHandler(webapp2.RequestHandler):
 		user_dict = userCollection[0].to_dict()
 		#check that secrets match
 		if (user_dict['stateXSRF'] == self.request.get('state')):
-			#payloadjson = {}
-			#payloadjson['code'] = self.request.get('code')
-			#payloadjson['client_id'] = clientId
-			#payloadjson['client_secret'] = clientSecret
-			#payloadjson['grant_type'] = 'authorization_code'
-			#payloadjson['redirect_uri'] = redirect2
-			#paystring = json.dumps(payloadjson)
-			paystring = "code=" + self.request.get('code') + "&client_id=" + clientId + "&client_secret=" + clientSecret + "&grant_type=authorization_code&redirect_uri=" + redirect2
-			result = urlfetch.fetch(url = "https://www.googleapis.com/oauth2/v4/token",payload=paystring,method=urlfetch.POST,headers={"Content-Type":"application/x-www-form-urlencoded"})
+			data = {'code': self.request.get('code'),'client_id': clientId,'client_secret': clientSecret,'redirect_uri': redirect2,'grant_type': 'authorization_code'}
+			result = urlfetch.fetch(url='https://googleapis.com/oauth2/v4/token', payload=urllib.urlencode(data),method=urlfetch.POST,headers={"Content-Type":"application/x-www-form-urlencoded"})
 			if (result.status_code == 200):
 				resultdict = result.content.to_dict()
 				userCollection[0].token = resultdict['access_token']
